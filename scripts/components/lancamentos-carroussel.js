514ec37b8985ff8367b8e95561b31f5e73fa475c
Ricardo <ricardo.kanashiro.adm@gmail.com>
2025-04-25 02:16:57 -0300

    fix(embla carrossel): corrigir bug no carrossel de produtos - não aplicando funcionalidade para todos

--- scripts/components/lancamentos-carroussel.js
+++ scripts/components/lancamentos-carroussel.js
@@ -1,13 +1,17 @@
 const initLancamentosCarroussel = () => {
-   const emblaNode = document.querySelector('.embla')
+   const emblaWrappers = document.querySelectorAll('.embla-wrapper')
    const options = { loop: false }
-   const emblaApi = EmblaCarousel(emblaNode, options)
 
-   const prevBtn = document.querySelector('.embla-wrapper__prev-btn')
-   const nextBtn = document.querySelector('.embla-wrapper__next-btn')
-
-   prevBtn.addEventListener('click', () => emblaApi.scrollPrev())
-   nextBtn.addEventListener('click', () => emblaApi.scrollNext())
+   emblaWrappers.forEach(wrapper => {
+      const emblaNode = wrapper.querySelector('.embla')
+      const emblaApi = EmblaCarousel(emblaNode, options)
+      
+      const prevBtn = wrapper.querySelector('.embla-wrapper__prev-btn')
+      const nextBtn = wrapper.querySelector('.embla-wrapper__next-btn')
+   
+      prevBtn.addEventListener('click', () => emblaApi.scrollPrev())
+      nextBtn.addEventListener('click', () => emblaApi.scrollNext())
+   })
 }
 
 export { initLancamentosCarroussel }
